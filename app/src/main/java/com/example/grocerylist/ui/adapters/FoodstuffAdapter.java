412c7c166ddc9588df70552aa6671a1bd25a86ec
nemanja.bozilovic <nemanja.bozilovic@outlook.com>
2025-01-03 13:08:28 +0100

    Code Refactoring

--- app/src/main/java/com/example/grocerylist/ui/adapters/FoodstuffAdapter.java
+++ app/src/main/java/com/example/grocerylist/ui/adapters/FoodstuffAdapter.java
@@ -2,7 +2,6 @@ package com.example.grocerylist.ui.adapters;
 
 import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
